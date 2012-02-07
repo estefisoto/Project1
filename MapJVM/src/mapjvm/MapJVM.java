67b3744a13a821851584420b7f22a669027ecff6
mwaldron74 <mwaldron74@yahoo.com>
2012-02-07 13:18:36 -0500

    commit

--- MapJVM/src/mapjvm/MapJVM.java
+++ MapJVM/src/mapjvm/MapJVM.java
@@ -21,29 +21,6 @@ public class MapJVM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Frame layout=new Frame("");
-        layout.addWindowListener(new closeWindow());
-        Canvas container= new Canvas();
-        container.setSize(800,800);
-        layout.add(container);
-       
-        MenuBar bar=new MenuBar();
-        Menu file = new Menu("File");
-        bar.add(file);
-        MenuItem connect = new MenuItem("Connect to JVM");
-        MenuItem close = new MenuItem("Close");
-        file.add(connect);
-        file.add(close);
-        layout.setMenuBar(bar);
-        
-      //  Scrollbar vertical= new Scrollbar();
-        
-      //  Scrollbar horizontal= new Scrollbar(Scrollbar.HORIZONTAL,1000,50,500,5000);
-      //  layout.add(vertical);
-      //  layout.add(horizontal);
-        
-        
-        layout.pack();
-        layout.setVisible(true); 
+        GUI window = new GUI(); 
     }
 }
