27aab69bb5a71793f72faf89664d92ce9ea965f9
Steve <stephen@ubuntu.ubuntu-domain>
2012-02-13 13:26:08 -0500

    Connects and Lists Locals

--- JVMConnector/src/jvmconnector/JVMConnector.java
+++ JVMConnector/src/jvmconnector/JVMConnector.java
@@ -4,9 +4,7 @@
  */
 package jvmconnector;
 
-import com.sun.jdi.Bootstrap;
-import com.sun.jdi.VirtualMachine;
-import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.*;
 import com.sun.jdi.connect.AttachingConnector;
 import com.sun.jdi.connect.Connector;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
@@ -23,7 +21,7 @@ public class JVMConnector {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException {
+    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException, InterruptedException, IncompatibleThreadStateException, AbsentInformationException {
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
         AttachingConnector socket = null;
         VirtualMachine vm = null;
@@ -37,14 +35,38 @@ public class JVMConnector {
                 break;
             }
         }
-        if(socket != null)
-        {
-            Map parametersMap = socket.defaultArguments();
-            Connector.IntegerArgument portArg = (Connector.IntegerArgument)parametersMap.get("port");
-            System.out.println(portArg);
-            portArg.setValue(8000);
-            vm = socket.attach(parametersMap);
-            System.out.println("Attached to Process '" + vm.name() + "'");
-        }
+
+        Map parametersMap = socket.defaultArguments();
+        Connector.IntegerArgument portArg = (Connector.IntegerArgument)parametersMap.get("port");
+        System.out.println(portArg);
+        portArg.setValue(8000);
+        vm = socket.attach(parametersMap);
+        vm.suspend();
+        System.out.println("Attached to Process '" + vm.name() + "'");
+
+        List<ThreadReference> ltr = vm.allThreads();
+            for(ThreadReference tr : ltr)
+            {
+                if(tr.name().equalsIgnoreCase("main"))
+                {
+                    ThreadReference tr2 = tr;
+                    System.out.println(tr);
+                    List<StackFrame> lsf = tr2.frames();
+                    System.out.println(lsf.size() + " Stack Frames");
+                    for(StackFrame sf : lsf)
+                    {
+                        try {
+                            List<LocalVariable> llv = sf.visibleVariables();
+                            for (LocalVariable lv : llv) 
+                            {
+                                System.out.println(lv);
+                            }
+                        } catch (AbsentInformationException aie) {
+                            System.out.println("No info for " + sf);
+                        }
+                    }
+                }
+                
+            }
     }
 }
