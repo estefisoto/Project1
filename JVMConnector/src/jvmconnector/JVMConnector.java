8b5ca6e7a65ee81c27ee98fbf0bcd2349bd952b4
Steve <stephen@ubuntu.ubuntu-domain>
2012-02-14 12:33:01 -0500

    Version Dos

--- JVMConnector/src/jvmconnector/JVMConnector.java
+++ JVMConnector/src/jvmconnector/JVMConnector.java
@@ -21,7 +21,7 @@ public class JVMConnector {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException, InterruptedException, IncompatibleThreadStateException, AbsentInformationException {
+    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException, InterruptedException, IncompatibleThreadStateException, AbsentInformationException, ClassNotLoadedException {
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
         AttachingConnector socket = null;
         VirtualMachine vm = null;
@@ -43,25 +43,44 @@ public class JVMConnector {
         vm = socket.attach(parametersMap);
         vm.suspend();
         System.out.println("Attached to Process '" + vm.name() + "'");
+        
+        List<ObjectReference> attempt = null;
 
         List<ThreadReference> ltr = vm.allThreads();
-            for(ThreadReference tr : ltr)
+            for(ThreadReference thread_ref : ltr)
             {
-                if(tr.name().equalsIgnoreCase("main"))
+                if(thread_ref.name().equalsIgnoreCase("main"))
                 {
-                    ThreadReference tr2 = tr;
-                    System.out.println(tr);
-                    List<StackFrame> lsf = tr2.frames();
-                    System.out.println(lsf.size() + " Stack Frames");
-                    for(StackFrame sf : lsf)
+                    ThreadReference main_tr = thread_ref;
+                    System.out.println(main_tr);
+                    List<StackFrame> sf_list = main_tr.frames();
+                    System.out.println(sf_list.size() + " Stack Frames");
+                    for(StackFrame sf : sf_list)
                     {
                         try {
-                            List<LocalVariable> llv = sf.visibleVariables();
-                            for (LocalVariable lv : llv) 
-                            {
-                                System.out.println(lv);
+                                List<LocalVariable> llv = sf.visibleVariables();
+                                for (LocalVariable lv : llv) 
+                                {
+                                    //System.out.println(lv.typeName());
+                                    //System.out.println(lv);
+                                    if(lv.type() instanceof ReferenceType)
+                                    {
+                                        ReferenceType rt = (ReferenceType) lv.type();
+                                        List<ObjectReference> objref_l = rt.instances(0);
+                                        for(ObjectReference objref : objref_l)
+                                        {                                       
+                                            List<ObjectReference> referringObj_l = objref.referringObjects(0);
+                                            System.out.println("******" + objref);
+                                            for(ObjectReference referring_obj : referringObj_l)
+                                            {
+                                                System.out.println("^^ referred to by: " + referring_obj);
+                                            }
+                                        }
+                                    }
+                                }
                             }
-                        } catch (AbsentInformationException aie) {
+                          
+                        catch (AbsentInformationException aie) {
                             System.out.println("No info for " + sf);
                         }
                     }
